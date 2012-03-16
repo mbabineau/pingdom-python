37fd7f23e13e621a6d4c4e510fe6b0e3b9212cf9
John Costa <john.costa@gmail.com>
2012-03-16 09:20:30 -0400

    pep8

--- setup.py
+++ setup.py
@@ -20,22 +20,23 @@ import sys
 
 import pingdom
 
-setup(name = 'pingdom',
-      version = pingdom.__version__,
-      description = 'Pingdom Library',
-      long_description='3rd-party Python interface to Pingdom\'s new REST API. Note that this API is still in beta. See README.rst for details',
-      author = 'Mike Babineau',
-      author_email = 'michael.babineau@gmail.com',
-      install_requires = ['simplejson'],
-      url = 'http://github.com/EA2D/pingdom-python',
-      packages = ['pingdom'],
-      license = 'Apache v2.0',
-      platforms = 'Posix; MacOS X; Windows',
-      classifiers = [ 'Development Status :: 3 - Alpha',
-                      'Intended Audience :: Developers',
-                      'Intended Audience :: System Administrators',
-                      'License :: OSI Approved :: Apache Software License',
-                      'Operating System :: OS Independent',
-                      'Topic :: System :: Monitoring',
-                      ]
+setup(name='pingdom',
+      version=pingdom.__version__,
+      description='Pingdom Library',
+      long_description="""3rd-party Python interface to Pingdom's new REST
+        API. Note that this API is still in beta. See README.rst for
+        details""",
+      author='Mike Babineau',
+      author_email='michael.babineau@gmail.com',
+      install_requires=['simplejson'],
+      url='http://github.com/EA2D/pingdom-python',
+      packages=['pingdom'],
+      license='Apache v2.0',
+      platforms='Posix; MacOS X; Windows',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: OS Independent',
+                   'Topic :: System :: Monitoring', ]
       )

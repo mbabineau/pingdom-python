68e98603b4c0a3e596794623011d82cb7a5d020b
John Costa <john.costa@gmail.com>
2012-03-16 09:53:30 -0400

    pep8

--- pingdom/resources.py
+++ pingdom/resources.py
@@ -9,13 +9,15 @@
 
 """
 
+
 class PingdomCheck(object):
     def __init__(self, attributes):
         for attr in attributes.keys():
             setattr(self, attr, attributes[attr])
-            
+
     def __repr__(self):
         return "Check:%s" % self.name
+
 
 class PingdomContact(object):
     def __init__(self, attributes):

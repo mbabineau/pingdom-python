2a7327853219192acf76d94acbdce7fec7b4dc5e
John Costa <john.costa@gmail.com>
2012-03-16 09:51:09 -0400

    pep8

--- pingdom/exception.py
+++ pingdom/exception.py
@@ -10,30 +10,35 @@
 """
 
 import gzip
+import StringIO
+import urllib2
+
 try:
     import json as simplejson
 except:
     import simplejson
-import StringIO
-import urllib2
+
 
 class PingdomError(urllib2.HTTPError):
     def __init__(self, http_error):
-        urllib2.HTTPError.__init__(self, http_error.filename, http_error.code, http_error.msg, http_error.hdrs, http_error.fp)
-        
+        urllib2.HTTPError.__init__(self, http_error.filename, http_error.code,
+            http_error.msg, http_error.hdrs, http_error.fp)
+
         if self.headers.get('content-encoding') == 'gzip':
-            data = gzip.GzipFile(fileobj=StringIO.StringIO(http_error.read())).read()
+            data = gzip.GzipFile(
+                fileobj=StringIO.StringIO(http_error.read())).read()
         else:
             data = self.read()
-        
+
         j = simplejson.loads(data)
         error = j['error']
         self.statuscode = error['statuscode']
         self.statusdesc = error['statusdesc']
         self.errormessage = error['errormessage']
-    
+
     def __repr__(self):
-        return 'PingdomError: HTTP %s %s returned with message, "%s"' % (self.statuscode, self.statusdesc, self.errormessage)
+        return 'PingdomError: HTTP %s %s returned with message, "%s"' % \
+               (self.statuscode, self.statusdesc, self.errormessage)
 
     def __str__(self):
         return self.__repr__()
